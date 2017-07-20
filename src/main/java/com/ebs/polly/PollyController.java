0c468ab2903f22f669526efbff62bcc93a937adf
Vitor Nunes Benedito <vitorbenedito@Vitors-MacBook-Pro.local>
2017-07-20 20:58:18 -0300

    Add cors filter˜

--- src/main/java/com/ebs/polly/PollyController.java
+++ src/main/java/com/ebs/polly/PollyController.java
@@ -8,17 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping("/polly")
 public class PollyController {
 	
 	@Autowired
 	private PollyService pollyService;
+	
 	
 	@PostMapping("/speech")
     public @ResponseBody String pollyURL(@RequestBody PollyText pollyText, HttpServletResponse response) throws IOException {
